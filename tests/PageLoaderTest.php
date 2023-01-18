b2123a4bba3220b1c7a39cd2934c8fc59a1faf03
Valeriy <95502737+VaLeraGav@users.noreply.github.com>
2023-01-18 12:57:12 +0300

    test: split the url search check

--- tests/PageLoaderTest.php
+++ tests/PageLoaderTest.php
@@ -4,6 +4,7 @@ namespace Tests;
 
 use App\PageLoader;
 use App\Utils\UrlUtils;
+use http\Encoding\Stream;
 use PHPUnit\Framework\TestCase;
 
 class PageLoaderTest extends TestCase
@@ -11,29 +12,50 @@ class PageLoaderTest extends TestCase
     private string $outputDir;
     private string $url;
     private string $unreachableAddress;
+    private PageLoader $pl;
+    private string $html;
 
     public function setUp(): void
     {
         $this->outputDir = __DIR__;
         $this->url = 'https://php.net';
         $this->unreachableAddress = 'http://test.test';
+
+        $this->pl = new PageLoader($this->unreachableAddress, $this->outputDir);
+        $this->html = (string)file_get_contents($this->outputDir . '/fixtures/mypage.html');
     }
 
-    public function testParsingHtml()
+    public function testLinksSearch()
     {
-        $html = (string)file_get_contents($this->outputDir . '/fixtures/test.html');
+        $html = (string)file_get_contents($this->outputDir . '/fixtures/mypage.html');
 
         $links = [
-            "https://test.test/assets/menu.css",
-            "/assets/application.css",
-            "/courses"
+            'https://mypage/assets/menu.css',
+            '/assets/application.css',
+            '/courses',
+            '/courses-1'
         ];
-        $images = ["/assets/php.png"];
-        $scripts = ["https://js.stripe.com/v3/", "https://test.test/packs/js.js"];
+        $this->assertEquals($links, $this->pl->getLinks($html));
+    }
 
-        $pl = new PageLoader($this->unreachableAddress, $this->outputDir);
-        $this->assertEquals($links, $pl->getLinks($html));
-        $this->assertEquals($images , $pl->getImages($html));
-        $this->assertEquals($scripts, $pl->getScripts($html));
+    public function testImagesSearch()
+    {
+        $images = [
+            'https://mypage/assets/img-2.png',
+            '/assets/img-1.png',
+            '/assets/img-2.png',
+        ];
+        $this->assertEquals($images, $this->pl->getImages($this->html));
+    }
+
+    public function testScriptsSearch()
+    {
+        $scripts = [
+            'https://js.stripe.com/v3/',
+            '//www.googletagservices.com/tag/js/gpt.js',
+            '/assets/index.js',
+            '/assets/index-1.js',
+        ];
+        $this->assertEquals($scripts, $this->pl->getScripts($this->html));
     }
 }
